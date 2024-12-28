8efa3508fcd725cb3dbb4f071088b9ddf0967e87
batux4n <batu.xan@hotmail.com>
2024-12-28 17:40:37 +0300

    First commit

---
+++ src/AboutSection.js
@@ -0,0 +1,11 @@
+function AboutSection() {
+    return (
+      <section id="about">
+        <h2>About Section</h2>
+        <p>This is the about section content.</p>
+      </section>
+    );
+  }
+  
+  export default AboutSection;
+  
